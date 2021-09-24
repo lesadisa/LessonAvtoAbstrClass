982084c38230445906383d4914dd10d55709c601
Igor <rtviwe@gmail.com>
2021-09-24 13:30:54 +0300

    Update Doorrun.java

--- src/main/java/door/Doorrun.java
+++ src/main/java/door/Doorrun.java
@@ -1,5 +1,6 @@
 package door;
 
+// TODO: немного странное название Doorrun, я бы назвал этот интерфейс просто Door с двумя методами open и close, так как любая дверь умеет открываться и закрываться
 public interface Doorrun {
 
         public String makeDorrun();

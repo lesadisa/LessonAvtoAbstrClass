320550218a430e4f18207f5c5457d925c5dcfaed
Igor <rtviwe@gmail.com>
2021-09-24 13:28:25 +0300

    Update Engine.java

--- src/main/java/engine/Engine.java
+++ src/main/java/engine/Engine.java
@@ -2,5 +2,6 @@ package engine;
 
 public interface Engine {
 
+    // TODO: я бы переименовал этот метод в например getSpeed, так как makeEngine больше похож на фабричный метод и мы ожидаем, что он вернет Engine, а не String
     public String makeEngine();
 }
